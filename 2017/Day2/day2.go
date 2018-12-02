b8c352734b338eecbdee2d1e7788eb7a1aad543d
Simon Danglmaier <s.danglmaier@protonmail.com>
2018-12-02 18:08:30 +0100

    Solved 2017 Day 2

--- 2017/Day2/day2.go
+++ 2017/Day2/day2.go
@@ -7,6 +7,8 @@ import "strings"
 import "strconv"
 
 func main() {
+
+	var input []string
 	var checksum uint64
 
 	//The Spreadsheet is loaded from a file and read line by line
@@ -21,13 +23,15 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+
+	for _, wholeLine := range input {
 		diff := 0
 
-		text := scanner.Text()
+		fmt.Printf("Line: %s \n", wholeLine)
 
-		fmt.Printf("Line: %s \n", text)
-
-		line := strings.Split(text, "\t")
+		line := strings.Split(wholeLine, "\t")
 
 		min, errmin := strconv.Atoi(line[0])
 		max, errmax := strconv.Atoi(line[0])
@@ -70,4 +74,40 @@ func main() {
 	}
 
 	fmt.Printf("The final checksum is: %d\n", checksum)
+
+	var sum int
+
+	for _, wholeLine := range input {
+
+		//fmt.Printf("Line: %s \n", wholeLine)
+
+		line := strings.Split(wholeLine, "\t")
+
+		for i := 0; i < len(line); i++ {
+			for j := 0; j < len(line); j++ {
+
+				if !(len(line[i]) == 0 || len(line[j]) == 0) {
+
+					num1, err1 := strconv.Atoi(line[i])
+					num2, err2 := strconv.Atoi(line[j])
+
+					if err1 != nil || err2 != nil {
+						fmt.Println("Something went wrong!")
+						return
+					}
+
+					//fmt.Printf("Comparing %d and %d\n", num1, num2)
+
+					if num1%num2 == 0 && num1 != num2 {
+						fmt.Printf("1: %d divides %d\n", num1, num2)
+						sum += num1 / num2
+						fmt.Printf("Sum is: %d\n", sum)
+					}
+
+				}
+			}
+		}
+	}
+
+	fmt.Printf("Sum for Part 2 is: %d\n", sum)
 }
