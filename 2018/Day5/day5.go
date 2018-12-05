1a63cdd3b647eee3e1b8c50a6138708943218f30
Simon Danglmaier <s.danglmaier@protonmail.com>
2018-12-05 20:03:50 +0100

    Implemented Part 2 of Day 5 2018

--- 2018/Day5/day5.go
+++ 2018/Day5/day5.go
@@ -4,10 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	var diffs uint = 1
+
+	var resPartTwo []string
 
 	//file, err := os.Open("input_test_1.txt")
 	file, err := os.Open("input.txt")
@@ -21,9 +24,11 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	scanner.Scan()
-	input := scanner.Text()
+	inputOriginal := scanner.Text()
 
-	fmt.Printf("Input: %s\n", input)
+	input := inputOriginal
+
+	//fmt.Printf("Input: %s\n", input)
 
 	for diffs > 0 {
 		diffs = 0
@@ -31,7 +36,7 @@ func main() {
 		for pos := 1; pos < len(input); pos++ {
 			if input[pos-1] == (input[pos]+32) || input[pos-1] == (input[pos]-32) {
 
-				fmt.Printf("Deleted %c and %c\n", input[pos-1], input[pos])
+				//fmt.Printf("Deleted %c and %c\n", input[pos-1], input[pos])
 				input = input[:pos-1] + input[pos+1:]
 				diffs++
 			}
@@ -39,4 +44,37 @@ func main() {
 	}
 
 	fmt.Printf("Result: %d\n", len(input))
+
+	for ch := 'A'; ch < 'Z'+1; ch++ {
+		diffs = 1
+		input = inputOriginal
+
+		input = strings.Replace(input, string(ch), "", -1)
+		input = strings.Replace(input, string(ch+32), "", -1)
+
+		for diffs > 0 {
+			diffs = 0
+
+			for pos := 1; pos < len(input); pos++ {
+				if input[pos-1] == (input[pos]+32) || input[pos-1] == (input[pos]-32) {
+
+					//fmt.Printf("Deleted %c and %c\n", input[pos-1], input[pos])
+					input = input[:pos-1] + input[pos+1:]
+					diffs++
+				}
+			}
+		}
+
+		resPartTwo = append(resPartTwo, input)
+	}
+
+	min := len(resPartTwo[0])
+
+	for pos := range resPartTwo {
+		if len(resPartTwo[pos]) < min {
+			min = len(resPartTwo[pos])
+		}
+	}
+
+	fmt.Printf("Shortest length is %d\n", min)
 }
