5f5401a1bce31901852ffd5da33edb5f15876ad1
Simon Danglmaier <s.danglmaier@protonmail.com>
2018-12-09 19:07:59 +0100

    Update Day 9 2018 to use lists, so part 2 can be done quickly

--- 2018/Day9/day9.go
+++ 2018/Day9/day9.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 )
+
+type testcase struct {
+	players int
+	marbles int
+	score   int64
+}
 
 func main() {
 
@@ -32,71 +39,78 @@ func main() {
 
 	//The real deal: Result: 384205
 	const players = 476
-	const marbles = 71431 * 10
+	const marbles = 71431 * 100
 
-	var board []int
+	var board = list.New()
 
 	var score [players]int
 
 	var max = score[0]
 	var maxIdx int
 
-	board = append(board, 0, 2, 1, 3)
+	board.PushBack(0)
+	board.PushBack(2)
+	board.PushBack(1)
+	board.PushBack(3)
 
-	fmt.Println(board)
+	var currMarbleIdx = board.Back()
 
-	var currMarble = 3
+	//fmt.Println(board)
+
+	//var currMarble = 3
 	var currPlayer = 3
 
 	for m := 4; m <= marbles; m++ {
-		var currMarbleIdx int
 
+		var idx *list.Element
 		currPlayer = (m % players)
 
-		//Find the index of the current marble
-		for i := range board {
-			if board[i] == currMarble {
-				currMarbleIdx = i
-				currMarble = m
-				break
-			}
-		}
+		idx = currMarbleIdx
 
 		if (m % 23) != 0 {
+			//Advance pointer by 2 places, loop to front if needed
 
-			idx := (currMarbleIdx + 2) % len(board)
+			for i := 0; i < 2; i++ {
+				if idx.Next() == nil {
+					idx = board.Front()
+				} else {
+					idx = idx.Next()
+				}
+			}
 
-			if idx == 0 {
-				board = append(board, m)
+			if idx == board.Front() {
+				board.PushBack(m)
+				currMarbleIdx = board.Back()
 			} else {
-
-				board = append(board, 0)
-
-				copy(board[idx+1:], board[idx:])
-
-				board[idx] = m
+				board.InsertBefore(m, idx)
+				currMarbleIdx = idx.Prev()
 			}
 		} else {
 
-			//actual length
-			act := 7 % len(board)
+			for i := 0; i < 7; i++ {
+				if idx.Prev() == nil {
+					idx = board.Back()
+				} else {
+					idx = idx.Prev()
+				}
+			}
 
-			idx := (currMarbleIdx - act + len(board)) % len(board)
-
-			score[currPlayer] += board[idx] + m
+			score[currPlayer] += idx.Value.(int) + m
 
 			if score[currPlayer] > max {
 				maxIdx = currPlayer
 				max = score[currPlayer]
 			}
 
-			copy(board[idx:], board[idx+1:])
-			board[len(board)-1] = 0
-			board = board[:len(board)-1]
+			if idx.Next() == nil {
+				currMarbleIdx = board.Front()
+			} else {
+				currMarbleIdx = idx.Next()
+			}
 
-			currMarble = board[idx]
-
+			board.Remove(idx)
 		}
+
 	}
 	fmt.Printf("Highest Score: %d; Player ID: %d\n", max, maxIdx)
 }
